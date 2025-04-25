bd8442689983686a24c0f522edb51cd29a946bc4
imt-eiaz <imteiaz@gmail.com>
2025-04-25 17:34:40 +0100

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+// import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div class="text-3xl font-bold text-teal-500">Welcome to EMS</div>;
+}
+
+export default App;
