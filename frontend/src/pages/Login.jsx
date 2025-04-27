442e79f46224a5c5f3fef1e635ebf8c091177774
Imtiaz Laptop <imteiaz@gmail.com>
2025-04-27 07:48:19 +0100

    2 ts 45

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        { email, password }
+      );
+      if (response.data.success) alert("Successfully login");
+      console.log(response);
+    } catch (error) {
+      if (error.response && !error.data.success) {
+        setError(error.response.data.error);
+      } else {
+        setError("Server Error");
+      }
+    }
+  };
   return (
     <div className="flex flex-col items-center h-screen justify-center bg-gradient-to-b from-green-700 from-50% to-gray-100 to-50% space-y-6">
       <h2 className="font-Dosis text-5xl mb-10 text-white">
@@ -8,8 +30,9 @@ const Login = () => {
       </h2>
       <div className="border shadow p-6 w-80 bg-white">
         <h2 className="text-2xl font-bold mb-4">Login</h2>
+        {error && <p className="text-red-500 ">{error}</p>}
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700">
               Email
@@ -18,6 +41,7 @@ const Login = () => {
               type="email"
               className="w-full px-3 py-2 border"
               placeholder="Enter Email"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-4">
@@ -28,6 +52,7 @@ const Login = () => {
               type="password"
               className="w-full px-3 py-2 border"
               placeholder="*******"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="mb-4 flex items-center justify-between">
