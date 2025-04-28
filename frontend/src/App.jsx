4bd8f293bdae46bd30375b89b09a33d727ebfff9
Imtiaz Laptop <imteiaz@gmail.com>
2025-04-28 18:13:09 +0100

    App is emphty all the routes has been appied in main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,3 @@
-import { BrowserRouter } from "react-router";
-
-import Login from "./pages/Login";
-import { Route, Navigate } from "react-router-dom";
-import { Routes } from "react-router";
-
-import AdminDashboard from "./pages/AdminDashboard";
-
-function App() {
-  <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
-      </Routes>
-    </BrowserRouter>
-    ;
-  </>;
-}
+function App() {}
 
 export default App;
