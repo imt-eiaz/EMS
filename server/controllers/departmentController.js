e8b06ef8108044aea29282f15a1dafc1e04cfbdd
imt-eiaz <imteiaz@gmail.com>
2025-05-16 17:09:05 +0100

    25.45

--- server/controllers/departmentController.js
+++ server/controllers/departmentController.js
@@ -1,3 +1,17 @@
-const addDepartment = (req, res) => {};
+import Department from "../models/Department.js";
 
+const addDepartment = async (req, res) => {};
+try {
+  const { dep_name, description } = req.body;
+  const newDep = new Department({
+    dep_name,
+    description,
+  });
+  await newDep.save();
+  return res.status(200).json({ success: true, department: newDep });
+} catch (error) {
+  return res
+    .status(500)
+    .json({ success: false, error: "add department server error" });
+}
 export { addDepartment };
--- server/routes/department.js
+++ server/routes/department.js
@@ -1,6 +1,9 @@
 import express from "express";
-import authMiddleware from "../middleware/auth.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+import { addDepartment } from "../controllers/departmentController.js";
 
-const router = express.router();
+const router = express.Router();
 
-router.post("/add", authMiddleware);
+router.post("/add", authMiddleware, addDepartment);
+
+export default router;
