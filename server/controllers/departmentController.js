01ec0132eb0c039f92854455b4e5b505da994a69
Imtiaz Laptop <imteiaz@gmail.com>
2025-05-17 09:10:00 +0100

    222

--- server/controllers/departmentController.js
+++ server/controllers/departmentController.js
@@ -1,17 +1,30 @@
 import Department from "../models/Department.js";
 
+// new code
 const addDepartment = async (req, res) => {};
-try {
-  const { dep_name, description } = req.body;
-  const newDep = new Department({
-    dep_name,
-    description,
-  });
-  await newDep.save();
-  return res.status(200).json({ success: true, department: newDep });
-} catch (error) {
-  return res
-    .status(500)
-    .json({ success: false, error: "add department server error" });
-}
+
+const { dep_name, description } = req.body;
+const newDep = new Department({
+  dep_name,
+  description,
+});
+
+await newDep.save();
+return res.status(200).json({ success: true, department: newDep });
+
 export { addDepartment };
+
+//old code
+// try {
+//   const { dep_name, description } = req.body;
+//   const newDep = new Department({
+//     dep_name,
+//     description,
+//   });
+//   await newDep.save();
+//   return res.status(200).json({ success: true, department: newDep });
+// } catch (error) {
+//   return res
+//     .status(500)
+//     .json({ success: false, error: "add department server error" });
+// }
